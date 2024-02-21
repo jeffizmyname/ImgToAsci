089d9ac137c476e1534e1d9612385aea175e03cd
jeffizmyname <80201005+jeffizmyname@users.noreply.github.com>
2024-02-21 20:19:25 +0100

    czytatamy zdjecie

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
 #include "opencv2/opencv.hpp"
 
+using namespace cv;
+using namespace std;
 
-int main() {
+int main()
+{
+
+    Mat image = imread("C:\\Users\\stasi\\Documents\\koding\\c++\\ImgToAsci\\src\\image.png", IMREAD_GRAYSCALE);
+
+    if (image.empty())
+    {
+        cout << "Image File "<< "Not Found" << endl;
+        cin.get();
+        return -1;
+    }
+
+    imshow("Window Name", image);
+
+    waitKey(0);
 
     return 0;
-
 }
